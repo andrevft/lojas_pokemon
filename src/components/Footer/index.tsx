ae05555f2b770c5ee974475d4572e4d2d1355a13
andrevft <a.fanchini@gmail.com>
2020-07-09 15:44:08 -0300

    netlify 1.2

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,17 +7,7 @@ const Footer = () => {
   return(
     <>
       <FooterContainer>
-        <p>
-          Feito com <FaHeart size={18} style={{ color: '#BE1931', margin: '0 0.1em' }} /> por
-          <HardLink href='https://www.linkedin.com/in/sndev'>Sinésio Neto <FaLinkedinIn style={{ marginBottom: '-2px' }} /></HardLink>
-        </p>
-        <Button
-          href='https://github.com/sineto/gama-pokestore'
-          target='_blank'
-        >
-          <FaGithub style={{ marginRight: '8px' }} />
-          Github Repository
-        </Button>
+       
       </FooterContainer>
     </>
   );
